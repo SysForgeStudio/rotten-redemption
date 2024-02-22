904ecec4b05e4cb58b6c0dc1320ae672ed210845
= <=>
2024-02-22 23:43:47 +0100

    Code refactor

--- Player/player.gd
+++ Player/player.gd
@@ -5,5 +5,4 @@ const SPEED = 600
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
 	velocity = direction * SPEED
-	print(global_position)
 	move_and_slide()
