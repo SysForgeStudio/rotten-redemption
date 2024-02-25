28fe32ff086c7b1cf76237540f1cb3b767559cf6
= <=>
2024-02-25 19:30:54 +0100

    Zombies turn to player

--- Mobs/mob.gd
+++ Mobs/mob.gd
@@ -1,9 +1,18 @@
 extends CharacterBody2D
 
-const SPEED = 900
+var speed = 900
 var speed_multiplier = 6
+var direction_after_change
+
 @onready var base = get_node("/root/World/Base")
+@onready var player = get_node("/root/World/Player")
 
 func _physics_process(delta):
-	velocity.x = SPEED * delta * speed_multiplier
+	velocity.x = speed * delta * speed_multiplier
+	move_and_slide()
+
+func _change_movement_to_player():
+	speed = 100
+	direction_after_change = global_position.direction_to(player.global_position)
+	velocity = direction_after_change * speed
 	move_and_slide()
--- World/world.gd
+++ World/world.gd
@@ -1,15 +1,21 @@
 extends Node2D
 
-func spawn_mob():
+func _spawn_mob():
 	var new_mob = preload("res://Mobs/mob.tscn").instantiate()
 	get_node("Path2D/PathFollow2D").progress_ratio = randf()
 	new_mob.global_position = get_node("Path2D/PathFollow2D").global_position
 	get_node("Mobs").add_child(new_mob)
 	
 
+func _apply_movement_on_children(node):
+	for child in node.get_children():
+		_apply_movement_on_children(child)
+		if child.has_method("_change_movement_to_player"):
+			child._change_movement_to_player()
+
 func _on_world_timer_timeout():
-	spawn_mob()
+	_spawn_mob()
 
 
 func _on_base_base_health_depleted():
-	pass
+	_apply_movement_on_children(get_node("Mobs"))
