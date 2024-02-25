81db06f544b33106858d3e9ee1b9d423cfb800c7
= <=>
2024-02-25 17:57:59 +0100

    Mob spawn

--- Mobs/mob.gd
+++ Mobs/mob.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
-const SPEED = 10
+const SPEED = 900
+var speed_multiplier = 6
 @onready var base = get_node("/root/World/Base")
 
 func _physics_process(delta):
-	velocity.x += SPEED * delta
+	velocity.x = SPEED * delta * speed_multiplier
 	move_and_slide()
---
+++ World/world.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+func spawn_mob():
+	var new_mob = preload("res://Mobs/mob.tscn").instantiate()
+	get_node("Path2D/PathFollow2D").progress_ratio = randf()
+	new_mob.global_position = get_node("Path2D/PathFollow2D").global_position
+	add_child(new_mob)
+
+
+func _on_world_timer_timeout():
+	spawn_mob()
