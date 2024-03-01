0db8d0567cf790903e50de7d92c7f7a8355f0714
= <=>
2024-03-01 15:23:07 +0100

    shooting animation

--- Player/player.gd
+++ Player/player.gd
@@ -1,15 +1,34 @@
 extends CharacterBody2D
 
 const SPEED = 600
+var shooting = false
+
+@onready var anim = get_node("AnimationPlayer")
 
 func _ready():
-	get_node("Girl").play("Idle")
+	if shooting == false:
+		get_node("Girl").play("Idle")
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
 	velocity = direction * SPEED
+	
+	if Input.is_action_just_pressed("shoot") and shooting == false:
+		shooting = true
+		anim.play("Shoot")
+		get_node("Timer").start()
+	
 	if velocity:
-		get_node("Girl").play("Run")
+		if shooting == false:
+			anim.play("Run")
 	else:
-		get_node("Girl").play("Idle")
+		if !shooting:
+			anim.play("Idle")
+
 	move_and_slide()
+
+
+func _on_timer_timeout():
+	shooting = false
+	anim.play("Idle")
+	get_node("Timer").wait_time = 0.4
