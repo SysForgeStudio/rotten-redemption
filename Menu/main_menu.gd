9f94068841977617b49563a2f95856d836aea92f
= <=>
2024-03-18 21:09:22 +0100

    Scene change menu->world

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
-
 func _on_play_button_pressed():
+	_screen_blackout()
+	await get_tree().create_timer(4.0).timeout
 	get_tree().change_scene_to_file("res://World/world.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _screen_blackout():
+	var color_tween = get_tree().create_tween()
+	var screen = get_node("CanvasLayer/NextScene")
+	screen.visible = true
+	color_tween.tween_property(screen, "modulate",Color.BLACK,4)
