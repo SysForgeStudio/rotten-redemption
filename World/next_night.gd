88ddeaced42e54bd43ce69f6b65136f697e07860
SysForgeStudio <sysforgestudio@gmail.com>
2024-04-19 14:14:19 +0200

    improvements, known issue

--- World/next_night.gd
+++ World/next_night.gd
@@ -4,7 +4,8 @@ var hours_base = 0
 var hours_supplies = 0
 
 func _ready():
-	pass
+	$CanvasLayer/Paper/ColorRectBase/LabelBaseHours.text = str(hours_base)
+	$CanvasLayer/Paper/ColorRectSupplies/LabelSuppliesHours.text = str(hours_supplies)
 
 func quit():
 	get_tree().quit()
@@ -18,3 +19,30 @@ func add_supplies():
 func repair_base():
 	pass
 
+func increase_hours_left(hours):
+	var temp = Game.hours_left
+	
+	
+	if(Game.hours_left > 0 and hours <= 12):
+		hours += 1
+		temp -= 1
+		Game.hours_left = temp
+	
+	return hours
+
+func decrease_base_hours(hours):
+	var temp = Game.hours_left
+	
+	if(Game.hours_left < 12 and hours > 0): #fix issue here
+		hours -= 1
+		temp += 1
+		Game.hours_left = temp
+
+func _on_button_up_base_pressed():
+	hours_base = increase_hours_left(hours_base)
+	$CanvasLayer/Paper/ColorRectBase/LabelBaseHours.text = str(hours_base)
+
+
+func _on_button_down_base_pressed():
+	hours_base = decrease_base_hours(hours_base)
+	$CanvasLayer/Paper/ColorRectBase/LabelBaseHours.text = str(hours_base)
